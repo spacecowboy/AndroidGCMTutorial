599d6fda70fbeaadbe2002fbf2fdd9baaed3873e
Jonas Kalderstam <jonas@kalderstam.se>
2013-09-19 21:49:18 +0200

    DB bits implemented. REST is complete??
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- server-app/app.py
+++ server-app/app.py
@@ -1,33 +1,76 @@
-import bottle
-from bottle import run, get, post, delete
+from hashlib import sha1
+from bottle import run, get, post, delete, install, HTTPError, request
+from bottle_sqlite import SQLitePlugin
+
+from dbsetup import init_db
+
+
+DBNAME='test.db'
+
+init_db(DBNAME)
+install(SQLitePlugin(dbfile=DBNAME))
+
+def to_dict(row):
+    return dict(sha=row['sha'],
+                url=row['url'],
+                timestamp=row['timestamp'])
 
 @get('/')
 @get('/links')
-def list_links():
+def list_links(db):
     '''Return a complete list of all links'''
-    return dict(links=[])
+    links = []
+    for row in db.execute('SELECT * from links'):
+        links.append(to_dict(row))
+    return dict(links=links)
 
-@get('/links/<id>')
-def get_link(id):
+@get('/links/<sha>')
+def get_link(db, sha):
     '''Returns a specific link'''
-    return dict(link={"sha":"1111111",
-                      "url":"http://www.google.com",
-                      "timestamp":"2013-09-19 08:22:19.000"})
+    row = db.execute('SELECT * from links WHERE sha IS ?', [sha]).fetchone()
+    if row:
+        return dict(link=to_dict(row))
 
-@delete('/links/<id>')
-def delete_link(id):
+    return HTTPError(404, "No such item")
+
+@delete('/links/<sha>')
+def delete_link(db, sha):
     '''Deletes a specific link from the list.
     On success, returns an empty response'''
-    return {}
+    db.execute('DELETE FROM links WHERE sha IS ?', [sha])
+    if db.total_changes > 0:
+        return {}
+
+    return HTTPError(404, "No such item")
 
 @post('/links')
-def add_link():
+def add_link(db):
     '''Adds a link to the list.
     On success, returns the entry created.'''
-    return dict(link={"sha":"1111111",
-                      "url":"http://www.google.com",
-                      "timestamp":"2013-09-19 08:22:19.000"})
+    # Only accept json data
+    if request.content_type != 'application/json':
+        return HTTPError(415, "Only json is accepted")
+    # Check required fields
+    if 'url' not in request.json:
+        return HTTPError(400, "Must specify a url")
+
+    # Sha is optional, generate if not present
+    if 'sha' not in request.json:
+        request.json['sha'] = sha1(request.json['url']).hexdigest()
+
+    args = [request.json['url'],
+            request.json['sha']]
+    if 'timestamp' in request.json:
+        args.append(request.json['timestamp'])
+        stmt = 'INSERT INTO links (url, sha, timestamp) VALUES(?, ?, ?)'
+    else:
+        stmt = 'INSERT INTO links (url, sha) VALUES(?, ?)'
+
+    db.execute(stmt, args)
+
+    return get_link(db, request.json['sha'])
+
 
 if __name__ == '__main__':
-    bottle.debug(True)
-    run(port=5500)
+    # Restart server automatically when this file changes
+    run(host='0.0.0.0', port=5500, reloader=True, debug=True)
--- server-app/curl_tests.sh
+++ server-app/curl_tests.sh
@@ -10,4 +10,11 @@ echo "\n\nGetting one"
 curl -X GET http://localhost:5500/links/42
 
 echo "\n\nAdding one"
-curl -X POST http://localhost:5500/links
+curl -X POST -H "Content-Type: application/json" -d '{"url":"http://www.google.com","username":"xyz","password":"xyz"}' http://localhost:5500/links
+
+echo "\n\nAdding wrong header"
+curl -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:5500/links
+
+
+echo "\n\nAdding no url"
+curl -X POST -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:5500/links
